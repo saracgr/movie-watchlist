9eedffa541ccb42f171e6e64344bcd3708ccbcca
saracgr <sarargcarvalho@gmail.com>
2025-05-17 17:21:50 +0100

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,13 +11,11 @@ export default function Login(){
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-       
               try {
-                const res = await axios.post(
+                    await axios.post(
                     'https://movie-watchlist-wwt5.onrender.com/login',
                     {username, password},
-                    { withCredentials: true }
-                    
+                    { withCredentials: true }                   
                 )
                 setUsername('');
                 setPassword('');
